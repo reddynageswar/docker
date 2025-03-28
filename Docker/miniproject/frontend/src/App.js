2954d3286544d4cdbc09c682d76ab0647a05a0c9
surya <mohanareddy1430@gmail.com>
2025-03-28 11:58:30 +0530

    Docker mini project, KT Documents and error list

---
+++ Docker/miniproject/frontend/src/App.js
@@ -0,0 +1,12 @@
+// frontend/src/App.js
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, this is docker class</h1>
+    </div>
+  );
+}
+
+export default App;
