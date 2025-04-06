859b6c4e0d2301c87a56daa1f8ccdff505cc68ca
SURIYAPRAKASAHREDDY <155240817+SURIYAPRAKASAHREDDY@users.noreply.github.com>
2025-04-06 22:43:16 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -14,6 +14,7 @@ pipeline {
                 echo "🔧 Running build steps..."
                 sh """
                   cd /var/lib/jenkins/workspace/new-jenkins/Docker/miniproject
+                  docker-compose -f docker-compose.yaml down -d
                   docker-compose -f docker-compose.yaml up -d
                  """
                 sh 'echo "Build successful!"'
